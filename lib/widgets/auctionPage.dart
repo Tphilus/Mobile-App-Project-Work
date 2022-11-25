6ff78c34c047371d1a63cbb63a089aa50615ce99
free-man11 <pfreeman@st.ug.edu.gh>
2022-11-25 17:38:31 +0000

    initial bro

--- lib/widgets/auctionPage.dart
+++ lib/widgets/auctionPage.dart
@@ -235,7 +235,7 @@ class _AuctionPageState extends State<AuctionPage> {
                       ),
                     ),
                     Text(
-                      '20h : 35min',
+                      '20h: 35min: 08s',
                       style: context.theme.textTheme.subtitle1?.copyWith(
                         color: context.colorScheme.background,
                         fontWeight: FontWeight.bold,
